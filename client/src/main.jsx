855c86622cf8b4182e21439b3d7610cbef9f1e3e
adidevlopersh <as1460177@gmail.com>
2026-01-03 14:30:25 +0530

    Hardcode backend URL for deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import axios from 'axios'
+
+// Set the base URL for axios requests
+// Hardcoded for reliability during deployment debugging
+axios.defaults.baseURL = 'https://itskillhub.onrender.com';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
